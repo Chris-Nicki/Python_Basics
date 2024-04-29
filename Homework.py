bbf59aaba6db423c1f60890e7e6ce6e53b616447
Chris-Nicki <christopher.nicki@gmail.com>
2024-04-29 15:29:19 -0600

    First Commit

---
+++ Homework.py
@@ -0,0 +1,41 @@
+# 1. Showcase Your Dance Moves!
+# Task 1. Correct the code
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+ 
+# Task 2. Your Mood Today
+mood = input("How are you feeling today?")
+print(f"I'm feeling {mood}")
+if mood == "happy":
+    print("Thats great to hear!")
+if mood == "sad":
+    print("I hope your day gets better")
+
+
+# 2. Python Naming Convention Practice
+# Task 1. Code correction
+pi_value = 3.14   
+user_age = 25
+USER_LOCATION = "New York"
+MAX_LIMIT = 1000
+
+
+# 3. Arithmetic Operations in Daily Life
+# Task 1. Grocery Store Math
+Coffee = 23
+Milk = 10
+Flour = 6
+
+Total_Cost = (Coffee + Milk + Flour)
+print(f"${Total_Cost}")
+
+# Task 2. Bank Interest
+Amount = 10000
+Interest = .07
+Total_Interest = (Amount * Interest)
+Year_End = (f"${int(Amount + Total_Interest)}")
+print(Year_End)
